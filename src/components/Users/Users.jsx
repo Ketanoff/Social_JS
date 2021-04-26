f62a1af9d7fc969534e0233865fdc2654b627e4a
Ketanoff <res.ddq@yandex.ru>
2021-04-26 16:52:01 +0900

    add pagination

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import * as axios from 'axios';
 import userPhoto from '../../assets/images/images1.png';
 import s from './Users.module.css';
+import {createPages} from './PageCreator';
 
 export class Users extends React.Component {
     
@@ -9,16 +10,34 @@ export class Users extends React.Component {
     //     super(props);
     // }
     componentDidMount() {
-        axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`).then(response => {
             this.props.setUsers(response.data.items);
+            this.props.setTotalUsersCount(response.data.totalCount);
         });
     }
     
+    onPageChanged = (pageNumber) => {
+        this.props.setCurrentPage(pageNumber);
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`).then(response => {this.props.setUsers(response.data.items);});
+    };
+    
     render() {
-        
+        let pagesCount = Math.ceil(this.props.totalUsersCount / this.props.pageSize);
+        let pages = [];
+        createPages(pages, pagesCount, this.props.currentPage);
+        // for (let i = 1; i <= pagesCount; i++) {
+        //     pages.push(i);
+        // }
         return <div className={s.users}>
-            {
-                this.props.users.map(u => <div key={u.id}>
+            <div>
+                {pages.map(p => {
+                    return <span className={this.props.currentPage === p ? s.selectedPage : s.cursor}
+                                 onClick={(e) => {this.onPageChanged(p);}}>{p}-</span>;
+                })}
+                <div>Total ({pagesCount}pages)</div>
+            
+            </div>
+            {this.props.users.map(u => <div key={u.id}>
                 <span>
                     <span>
                         <img src={u.photos.small != null ? u.photos.small : userPhoto}/>
@@ -29,7 +48,7 @@ export class Users extends React.Component {
                             : <button onClick={() => {this.props.follow(u.id);}}>Follow</button>}
                     </div>
                 </span>
-                    <span>
+                <span>
                     <span>
                         <div>{u.name}</div>
                         <div>{u.status}</div>
@@ -39,7 +58,7 @@ export class Users extends React.Component {
                         <div>{'u.location.country'}</div>
                     </span>
                 </span>
-                </div>)
+            </div>)
             }
         </div>;
         
--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,24 +1,33 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import {Users} from './Users';
-import {followAC, setUsersAC, unFollowAC} from '../../redux/users-reducer';
+import {followAC, setCurrentPageAC, setTotalUsersCountAC, setUsersAC, unFollowAC} from '../../redux/users-reducer';
 
 let mapStateToProps = (state) => {
     return {
-        users: state.usersPage.users
+        users          : state.usersPage.users,
+        pageSize       : state.usersPage.pageSize,
+        totalUsersCount: state.usersPage.totalUsersCount,
+        currentPage    : state.usersPage.currentPage
     };
 };
 
 let mapDispatchToProps = (dispatch) => {
     return {
-        setUsers: (users) => {
+        setUsers          : (users) => {
             dispatch(setUsersAC(users));
         },
-        follow  : (userId) => {
+        follow            : (userId) => {
             dispatch(followAC(userId));
         },
-        unFollow: (userId) => {
+        unFollow          : (userId) => {
             dispatch(unFollowAC(userId));
+        },
+        setCurrentPage    : (pageNumber) => {
+            dispatch(setCurrentPageAC(pageNumber));
+        },
+        setTotalUsersCount: (totalCount) => {
+            dispatch(setTotalUsersCountAC(totalCount));
         }
     };
 };
